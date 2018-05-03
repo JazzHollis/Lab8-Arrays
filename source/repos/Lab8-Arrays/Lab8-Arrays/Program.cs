2a8eb78d6a0e68e0c3f779b65a262b687e125a48
Jasmine Hollis <jasmine.hollis@rocketmail.com>
2018-05-03 17:45:10 -0400

    Second commit of Lab 8

--- source/repos/Lab8-Arrays/Lab8-Arrays/Program.cs
+++ source/repos/Lab8-Arrays/Lab8-Arrays/Program.cs
@@ -10,69 +10,66 @@ namespace Lab8_Arrays
     {
         static void Main(string[] args)
         {
-            int index = int.Parse(Console.ReadLine());
-            while (!int.TryParse(Console.ReadLine(), out index) || index < 1 || index >20)
-                
 
-            {
-
-                //Place while loop after strings
-
-                //try not to return multiple variables for methods
-
-
-                //1. Set Up Database
-                Console.WriteLine("Welcome to C# class. Which student would you " +
-                    "like to learn more about?enter a number 1-20");
-
-              
-
-                string[] Students = { "Kim", "Dorain", "Jackson", "Laura", "Daphne", "Marcellus", "Shaniqua","John",
+            string[] Students = { "Kim", "Dorain", "Jackson", "Laura", "Daphne", "Marcellus", "Shaniqua","John",
                                   "Josiah","Jacob", "Alexis", "Cynthia", "Mark", "Chad", "Doris", "Rhonda", "Destiny", "Isaiah", "Katelyn", "Zamira"};
 
 
-                string[] Hometown = { "Detroit","Seattle", "Chicago", "Atlanta", "New York", "St. Clair Shores", "Beverly Hills",
+            string[] Hometown = { "Detroit","Seattle", "Chicago", "Atlanta", "New York", "St. Clair Shores", "Beverly Hills",
                                   "Southfield", "Nashville", "Sacremento", "Phoenix", "Rio de Janeiro", "Paris", "Dallas",
                                   "Los Angeles", "Lagos","Washington D.C.", "Seattle", "Detroit", "Denver"};
 
-                string[] FavoriteFood = { "Pizza", "Nachos", "Chicken Alfredo", "Greek Salad", "Black Bean Burger", "Spaghetti", "Quiche",
+            string[] FavoriteFood = { "Pizza", "Nachos", "Chicken Alfredo", "Greek Salad", "Black Bean Burger", "Spaghetti", "Quiche",
                                       "Raviolli", "Samosas", "Lentil Kibbee","Fufu", "Taboulli", "BBQ Ribs", "Corn on the Cob",
                                       "Chimichanga", "Cole Slaw", "BLT sandwich", "Grinder Sub", "Chicken Salad"};
-
-
-                Console.WriteLine("What would you like to know about " + Students[index - 1] + " (enter 'h' for hometown and 'f' for favorite food");
-                string option1 = Console.ReadLine();
-
-                if (option1.ToLower().Contains("h"))
+            bool repeat = true;
+            while (repeat)
+            {
+                Console.WriteLine("Welcome to C# class. Which student would you " +
+                "like to learn more about?enter a number 1-20");
+                int index;
+                while (!int.TryParse(Console.ReadLine(), out index) || index < 1 || index > 20)
                 {
-                    Console.WriteLine(Students[index - 1] + "'s" + " hometown is " + Hometown[index - 1]);
+                    Console.WriteLine("Invlaid input! Please enter a number 1-20");
                 }
-
-                else if (option1.ToLower().Contains("f"))
+                while (true)
                 {
-                    Console.WriteLine(Students[index - 1] + "'s " + "favorite food is " + FavoriteFood[index - 1]);
-                }
+                    Console.WriteLine("What would you like to know about " + Students[index - 1] + " (enter 'h' for hometown and 'f' for favorite food");
+                    string option1 = Console.ReadLine();
 
-                else
-                {
-                    Console.WriteLine("Invalid input! Please try again!");
-                }
+                    if (option1.ToLower().Contains("h"))
+                    {
+                        Console.WriteLine(Students[index - 1] + "'s" + " hometown is " + Hometown[index - 1]);
+                        break;
+                    }
 
-                Console.WriteLine("Would you like to select another student?(y/n)");
-                string choice = Console.ReadLine();
-                if (choice == "n")
-                {
-                    Console.WriteLine("Goodbye");
-                    break;
+                    else if (option1.ToLower().Contains("f"))
+                    {
+                        Console.WriteLine(Students[index - 1] + "'s " + "favorite food is " + FavoriteFood[index - 1]);
+                        break;
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Invalid input! Please try again!");
+                    }
+
                 }
+                    Console.WriteLine("Would you like to select another student?(y/n)");
+                    string choice = Console.ReadLine();
+                    if (choice == "n")
+                    {
+                        Console.WriteLine("Goodbye");
+                        repeat = false;
+                    }
+
+                
 
             }
-               
+
+
+
         }
-
-
-
-    }
 
 
 
@@ -85,6 +82,7 @@ namespace Lab8_Arrays
 
         //5.Print information
 
+    }
 }
 
         
